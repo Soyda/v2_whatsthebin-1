05ded520bc80d78d81d2792efa966c5879d5169a
ayoub <ayoubhaddou1@gmail.com>
2022-06-17 16:14:59 +0200

    reduc requirements

--- App/routes.py
+++ App/routes.py
@@ -2,6 +2,12 @@ from App import app
 from flask import render_template
 
 
+def compute_item():
+    compute = 2 * 2 
+    return compute 
+
+
 @app.route('/')
 def homepage():
-    return render_template("home.html")
+    chiffre = compute_item()
+    return render_template("home.html", chiffre=chiffre)
