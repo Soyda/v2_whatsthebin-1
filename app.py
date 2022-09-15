33ecb2177c4d853bdf1ed5158d7a91b0e13bfe72
dumba-arnold <dumba.monga@gmail.com>
2022-09-15 10:04:40 +0200

    my feature on app

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+#the new things
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, redirect, url_for
+from flask_dance.contrib.google import make_google_blueprint, google
+#ça finit ici
+import logging
 from project import app
+
+load_dotenv()
+app= Flask(__name__)
+client_id = os.getenv('GOOGLE_CLIENT_ID')
+client_secret = os.getenv('GOOGLE_CLIENT_SECRET')
+app.secret_key=os.getenv('secret_key')
 
 if __name__ == "__main__":
     app.run(debug=True)
